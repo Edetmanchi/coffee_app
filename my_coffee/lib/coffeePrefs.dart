86a7a234770b141ad7cec810c997527b520a3ebf
Edet Effiong <greatstory247@gmail.com>
2025-01-09 00:00:09 +0100

    bug fixed

--- my_coffee/lib/coffeePrefs.dart
+++ my_coffee/lib/coffeePrefs.dart
@@ -39,3 +39,55 @@ class CoffeePrefs extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class CoffeePrefs extends StatelessWidget {
+  const CoffeePrefs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          Row(
+            children: [
+              const Text('Strengt:',style: TextStyle(fontSize: 20),),
+              Image.asset('assets/img/coffee_bean.png',
+              width: 40,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,             
+              ),
+              const SizedBox(width: 50,),
+              const Text('3',style: TextStyle(fontSize: 20),),
+            ],
+          ),
+          Row(
+            children: [
+              const Text('Sugar:',style: TextStyle(fontSize: 20),),
+              Image.asset('assets/img/sugar_cube.png',
+              width: 40,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,),
+              const SizedBox(width: 50,),
+              const Text('3',style: TextStyle(
+                fontSize: 20
+              ),),
+            ],
+
+          )
+         
+        ],
+
+    );
+  }
+}
